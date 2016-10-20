1548b9be6b1969e42f425d1a4b9e86a99514e79f
ppgmz333 <ie697875@iteso.mx>
2016-10-20 16:25:43 -0500

    V0.211 RTC header & source

--- MCP7940M.c
+++ MCP7940M.c
@@ -7,20 +7,116 @@
 
 #include "MCP7940M.h"
 
+uint8 RTC_write(uint8 address, uint8 data){
+	I2C_start();
+	I2C_write_Byte(CONTROL_W);
+	I2C_wait();
+	I2C_get_ACK();
 
-uint8 RTC_write(RTC_ConfigType* config);
+	I2C_writeByte(address);
+	I2C_wait();
+	I2C_get_ACK();
 
-uint8 RTC_writeHour(RTC_ConfigType* config);
+	I2C_writeByte(data);
+	I2C_wait();
+	I2C_get_ACK();
 
-uint8 RTC_writeDate(RTC_ConfigType* comfig);
+	I2C_stop();
 
-uint8 RTC_readHour();
+	return TRUE;
+}
 
-uint8 RTC_readDate();
+uint8 RTC_read(uint8 address){
+	static uint8 dataFromMCP7940M;
+	I2C_start();
 
-uint8 RTC_setAlarm1(RTC_ConfigType* alarm0);
+	I2C_write_Byte(CONTROL_W);
+	I2C_wait();
+	I2C_get_ACK();
 
-uint8 RTC_setAlarm2(RTC_ConfigType* alarm1);
+	I2C_writeByte(address);
+	I2C_wait();
+	I2C_get_ACK();
+
+	I2C_repeted_Start();
+	I2C_write_Byte(CONTROL_R);
+	I2C_wait();
+	I2C_get_ACK();
+
+	I2C_TX_RX_Mode(I2C_RX_MODE);
+
+
+	I2C_NACK();
+	dataFromMCP7940M = I2C_read_Byte();
+	I2C_NACK();
+
+	I2C_stop();
+	dataFromMCP7940M = I2C_read_Byte();
+
+	return readByte;
+}
+
+uint8 RTC_writeHour(RTC_ConfigType* config){
+	RTC_write(RTCSEC, config->second);
+	RTC_write(RTCMIN, config->minute);
+	RTC_write(RTCHOUR, ((config->format << 6) || config->hour));
+
+	return TRUE;
+}
+
+uint8 RTC_writeDate(RTC_ConfigType* config){
+	RTC_write(RTCWKDAY, config->weekday);
+	RTC_write(RTCDATE, config->date);
+	RTC_write(RTCMTH, config->month);
+	RTC_write(RTCYEAR, config->year);
+
+	return TRUE;
+}
+
+uint8 RTC_readHour(RTC_ConfigType* read){
+	read->second = RTC_read(RTCSEC);
+	read->minute = RTC_read(RTCMIN);
+	read->hour = RTC_read(RTCHOUR);
+
+	return TRUE;
+}
+
+uint8 RTC_readDate(){
+	read->weekday = RTC_read(RTCWKDAY);
+	read->date = RTC_read(RTCDATE);
+	read->month = RTC_read(RTCMTH);
+	read->year = RTC_read(RTCYEAR);
+
+	return TRUE;
+}
+
+uint8 RTC_setAlarm0(RTC_ConfigType* alarm0){
+	//enable alarm ----
+
+	RTC_write(ALM0SEC, alarm0->second);
+	RTC_write(ALM0MIN, alarm0->minute);
+	RTC_write(ALM0HOUR, ((alarm0->format << 6) || alarm0->hour));
+	RTC_write(ALM0WKDAY, alarm0->weekday);
+	RTC_write(ALM0DATE, alarm0->date);
+	RTC_write(ALM0MTH, alarm0->month);
+	RTC_write(ALM0YEAR, alarm0->year);
+
+	return TRUE;
+}
+
+uint8 RTC_setAlarm1(RTC_ConfigType* alarm1){
+	//enable alarm --
+
+	RTC_write(ALM1SEC, alarm1->second);
+	RTC_write(ALM1MIN, alarm1->minute);
+	RTC_write(ALM1HOUR, ((alarm1->format << 6) || alarm1->hour));
+	RTC_write(ALM1WKDAY, alarm1->weekday);
+	RTC_write(ALM1DATE, alarm1->date);
+	RTC_write(ALM1MTH, alarm1->month);
+	RTC_write(ALM1YEAR, alarm1->year);
+
+	return TRUE;
+}
 
 uint8 RTC_disableAlarm0();
 
--- MCP7940M.h
+++ MCP7940M.h
@@ -9,6 +9,36 @@
 #define SOURCES_MCP7940M_H_
 
 #include "DataTypeDefinitions.h"
+#include "I2C.h"
+
+#define CONTROL_W 1101110
+#define CONTROL_R 1101111
+
+#define RTCSEC 0x0
+#define RTCMIN 0x1
+#define RTCHOUR 0x2
+#define RTCWKDAY 0x3
+#define RTCDATE 0x4
+#define RTCMTH 0x5
+#define RTCYEAR 0x6
+
+#define ALxMEN 0x7
+#define AL0MEN 4
+#define AL1MEN 5
+
+#define ALM0SEC 0xA
+#define ALM0MIN 0xB
+#define ALM0HOUR 0xC
+#define ALM0WKDAY 0xD
+#define ALM0DATE 0xE
+#define ALM0MTH 0xF
+
+#define ALM1SEC 0x11
+#define ALM1MIN 0x12
+#define ALM1HOUR 0x13
+#define ALM1WKDAY 0x14
+#define ALM1DATE 0x15
+#define ALM1MTH 0x16
 
 typedef struct{
 	uint8 weekday :3;
@@ -22,19 +52,19 @@ typedef struct{
 }RTC_ConfigType;
 
 
-uint8 RTC_write(RTC_ConfigType* config);
+uint8 RTC_write(uint8 address, uint8 data);
 
 uint8 RTC_writeHour(RTC_ConfigType* config);
 
-uint8 RTC_writeDate(RTC_ConfigType* comfig);
+uint8 RTC_writeDate(RTC_ConfigType* config);
 
 uint8 RTC_readHour();
 
 uint8 RTC_readDate();
 
-uint8 RTC_setAlarm1(RTC_ConfigType* alarm0);
+uint8 RTC_setAlarm0(RTC_ConfigType* alarm0);
 
-uint8 RTC_setAlarm2(RTC_ConfigType* alarm1);
+uint8 RTC_setAlarm1(RTC_ConfigType* alarm1);
 
 uint8 RTC_disableAlarm0();
 
